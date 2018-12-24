f0641a2c315cd546000183265abe0378b533042e
paborte <pablo.ortega@alumnos.uva.es>
2018-12-24 18:21:44 +0100

    Añadidos los stubs de anadirPruebas de forma individual a una asignatura

--- src/main/java/es/uva/inf/tds/entornoeducativo/Asignatura.java
+++ src/main/java/es/uva/inf/tds/entornoeducativo/Asignatura.java
@@ -1,11 +1,27 @@
 package es.uva.inf.tds.entornoeducativo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Asignatura {
 	
 	public Asignatura (String nombre, String descripcion, int calificacionMaxima, LocalDate diaInicio, LocalDate diaFin) {
 		//TODO 
+	}
+
+	public void añadePrueba(Prueba pruebaAux, double pesoEnLaAsignatura) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ArrayList<Prueba> getPruebas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void añadePrueba(LocalDate diaDeLaPrueba, String NombreDeLaPrueba, String descripcionDeLaPrueba, double notaMax, double pesoEnLaAsignatura) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
