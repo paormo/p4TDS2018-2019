461557e8fbc3cdd7276c205dfec1ce056e354bdc
paborte <pablo.ortega@alumnos.uva.es>
2018-12-27 17:26:56 +0100

    Modificada pipeline de gitlab para pasar qa en cualquier rama. Modificada la suite para lanzar todos los tests. Modificados los test de secuencia que estaban mal elaborados

--- src/test/java/testsrc/AsignaturaSecuenciaTests.java
+++ src/test/java/testsrc/AsignaturaSecuenciaTests.java
@@ -16,7 +16,7 @@ public class AsignaturaSecuenciaTests {
 	public void testSecuenciaAsignatura1() {
 		Asignatura asignatura = new Asignatura("Matematicas", "Hola que tal", 3, LocalDate.now(), LocalDate.now().plusDays(10));
 		asignatura.añadePrueba(LocalDate.now().plusDays(5), "Examen1", "Por los loles", 0.3,5);
-		asignatura.añadePrueba(LocalDate.now().plusDays(2),"Examen 2", "Por esparta", 0.3,5);
+		asignatura.añadePrueba(LocalDate.now().plusDays(2),"Examen 2", "Por esparta", 0.7,5);
 		for (Prueba p : asignatura.getPruebas()) {
 			p.calificar("Alumno1", 5, LocalDate.now().plusDays(6));
 		}
